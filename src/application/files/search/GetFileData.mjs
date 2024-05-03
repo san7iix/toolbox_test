442367ead9f2bc8a342ee4cb31225acdbb1dd36f
Santiago Martínez <xsantysxx@gmail.com>
2024-05-03 17:54:55 -0500

    feat: Mejora de rendimiento en la ruta de obtención de archivos

--- src/application/files/search/GetFileData.mjs
+++ src/application/files/search/GetFileData.mjs
@@ -1,22 +1,25 @@
-import CustomFile from "../../../domain/File.mjs"
 import { parseFileData } from "../../../infraestructure/helpers/FUNCTIONS.mjs"
 import { getFileDataFromExternalAPIService } from "../../../infraestructure/service/FileServices.mjs"
 
 export const GetFileData = async (req, res) => {
 
-    const { fileName } = req.query
+    try {
+        const { fileName } = req.query
 
-    if (!fileName) {
-        res.status(400).json({ error: 'fileName is required' })
-        return
+        if (!fileName) {
+            res.status(400).json({ error: 'fileName is required' })
+            return
+        }
+
+        const fileData = await getFileDataFromExternalAPIService(fileName)
+
+        const dataResponse = []
+
+        parseFileData(fileData, dataResponse, fileName)
+
+        res.status(200).json(dataResponse)
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' })
     }
-
-    const fileData = await getFileDataFromExternalAPIService(fileName)
-
-    const dataResponse = []
-
-    parseFileData(fileData, dataResponse, fileName)
-
-    res.status(200).json(dataResponse)
 
 }
